22d2b98984c6affc5558061e395c7a8f28b973bd
SHUBHANKAR YASH <shubhankar.yash@flipkart.com>
2018-02-01 19:04:35 +0530

    Code cleanup

--- youtube-ios/Example/InlineYoutubeView/YTVideoCell.m
+++ youtube-ios/Example/InlineYoutubeView/YTVideoCell.m
@@ -9,12 +9,15 @@
 #import <Foundation/Foundation.h>
 #import "YTVideoCell.h"
 
+//Constants which decide whether to play the video inline or in fullscreen
 NSString *const INLINE = @"INLINE";
 NSString *const FULLSCREEN = @"FULLSCREEN";
 
+//The url where the HTML is hosted
 NSString *const HTML_URL = @"https://cdn.rawgit.com/flipkart-incubator/inline-youtube-view/60bae1a1/youtube-android/youtube_iframe_player.html";
 NSString *const PLAY_ICON = @"playIcon";
 
+//Dimension of the play icon used over the thumbnail
 CGFloat const PLAY_ICON_DIMENSION = 48;
 
 
@@ -26,43 +29,70 @@ CGFloat const PLAY_ICON_DIMENSION = 48;
 -(id)initWithFrame:(CGRect)frame andVideoId: (NSString *)videoId andShouldPlayInline: (BOOL) playInline {
     self = [super initWithFrame:frame];
     self.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    //Assigning the video properties
     self.videoId = videoId;
     self.playInline = playInline;
     
+    [self setupThumbnailWithFrame:frame];
+    return self;
+}
+    
+    
+-(void) setupThumbnailWithFrame: (CGRect)frame {
     self.thumbnailView = [[UIImageView alloc] initWithFrame:frame];
     self.thumbnailView.contentMode = UIViewContentModeScaleToFill;
+    [self superImposePlayIconOnThumbnail];
+    [self asynchronouslyDownloadThumbnailImage];
+}
     
+    
+-(void) superImposePlayIconOnThumbnail {
+    //Setup the playIconView and place it on top of the thumbnail image
     UIImage *playIconImage = [UIImage imageNamed:PLAY_ICON];
     self.playIconView = [[UIImageView alloc] initWithImage:playIconImage];
     [self.playIconView setFrame:CGRectMake(self.thumbnailView.bounds.size.width/2 - PLAY_ICON_DIMENSION/2, self.thumbnailView.bounds.size.height/2 - PLAY_ICON_DIMENSION/2, PLAY_ICON_DIMENSION, PLAY_ICON_DIMENSION)];
     [self.thumbnailView addSubview:self.playIconView];
-    
     [self addSubview:self.thumbnailView];
+}
     
-    NSString *urlString = [NSString stringWithFormat:@"https://img.youtube.com/vi/%@/hqdefault.jpg", videoId];
+    
+-(void)asynchronouslyDownloadThumbnailImage {
+    NSString *urlString = [NSString stringWithFormat:@"https://img.youtube.com/vi/%@/hqdefault.jpg", _videoId];
     NSURL *thumbnailUrl = [[NSURL alloc] initWithString:urlString];
-    dispatch_async( dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0 ), ^(void)
-                   {
-                       NSData * data = [[NSData alloc] initWithContentsOfURL:thumbnailUrl];
-                       UIImage * image = [[UIImage alloc] initWithData:data];
-                       dispatch_async( dispatch_get_main_queue(), ^(void){
-                           if(image != nil)
-                           {
-                               [self.thumbnailView setImage:image];
-                           }
-                       });
-                   });
-    return self;
+    dispatch_async( dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0 ), ^(void) {
+        NSData * data = [[NSData alloc] initWithContentsOfURL:thumbnailUrl];
+        UIImage * image = [[UIImage alloc] initWithData:data];
+        dispatch_async( dispatch_get_main_queue(), ^(void){
+            if(image != nil)
+            {
+                [self.thumbnailView setImage:image];
+            }
+        });
+    });
 }
     
     
 -(void) playButtonClicked {
     [self.playIconView removeFromSuperview];
+    
+    [self addLoaderOverThumbnail];
+    [self setupYoutubeView];
+    
+    //Wait for youtube player to to get ready or proceed if it is ready
+    if([self.youtubeView loadYTIframe]) {
+        [self playerViewDidBecomeReady:self.youtubeView];
+    }
+}
+    
+-(void)addLoaderOverThumbnail {
     self.loaderView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
     [self.loaderView setFrame:CGRectMake(self.thumbnailView.bounds.size.width/2 - PLAY_ICON_DIMENSION/2, self.thumbnailView.bounds.size.height/2 - PLAY_ICON_DIMENSION/2, PLAY_ICON_DIMENSION, PLAY_ICON_DIMENSION)];
     [self.loaderView startAnimating];
     [self.thumbnailView addSubview:self.loaderView];
+}
     
+-(void)setupYoutubeView {
     if(_playInline) {
         self.youtubeView = [[InlineYoutubeView alloc] initWithHtmlUrl:HTML_URL andVideoPlayerMode:INLINE];
     } else {
@@ -71,17 +101,26 @@ CGFloat const PLAY_ICON_DIMENSION = 48;
     
     self.youtubeView.frame = self.bounds;
     self.youtubeView.delegate = self;
-    
-    if([self.youtubeView loadYTIframe]) {
-        [self playerViewDidBecomeReady:self.youtubeView];
-    }
 }
     
+/*
+ InlineYoutubeView callbacks
+ */
+
+    
 - (void)playerViewDidBecomeReady:(nonnull InlineYoutubeView *)playerView {
+    [self removeLoaderFromThumbnail];
+    [self loadVideo:playerView];
+}
+    
+-(void) removeLoaderFromThumbnail {
     [self.loaderView stopAnimating];
     [self.loaderView removeFromSuperview];
-    [self addSubview:playerView];
+}
     
+    
+-(void) loadVideo: (nonnull InlineYoutubeView *)playerView {
+    [self addSubview:playerView];
     [playerView loadVideoById:_videoId startSeconds:0 suggestedQuality:kYTPlaybackQualityAuto];
     [playerView playVideo];
 }
