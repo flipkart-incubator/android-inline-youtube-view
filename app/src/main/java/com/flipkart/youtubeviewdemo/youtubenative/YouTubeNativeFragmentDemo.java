c052b3f9b43487c733b50167f72109979681f9c5
anirudhramanan <anirudh.r@flipkart.com>
2018-11-17 09:48:00 +0530

    licensing header to demo app files

--- app/src/main/java/com/flipkart/youtubeviewdemo/youtubenative/YouTubeNativeFragmentDemo.java
+++ app/src/main/java/com/flipkart/youtubeviewdemo/youtubenative/YouTubeNativeFragmentDemo.java
@@ -1,3 +1,21 @@
+/*
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+ *
+ * Copyright (c) 2018 Flipkart Internet Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.flipkart.youtubeviewdemo.youtubenative;
 
 import android.os.Bundle;
@@ -33,7 +51,7 @@ public class YouTubeNativeFragmentDemo extends Fragment {
         videoIds.add("ebXbLfLACGM");
         videoIds.add("mWRsgZuwf_8");
 
-        YouTubePlayerAdapter youTubePlayerAdapter = new YouTubePlayerAdapter(getContext(), videoIds, this, playerType);
+        YouTubePlayerAdapter youTubePlayerAdapter = new YouTubePlayerAdapter(videoIds, this, playerType);
         view.setAdapter(youTubePlayerAdapter);
 
         return view;
--- inline-youtube-view/src/main/java/com/flipkart/youtubeview/util/$Precondition$Check.java
+++ inline-youtube-view/src/main/java/com/flipkart/youtubeview/util/$Precondition$Check.java
@@ -1,12 +1,24 @@
+/*
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+ *
+ * Copyright (c) 2018 Flipkart Internet Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package com.flipkart.youtubeview.util;
 
 public final class $Precondition$Check {
-    public static void checkArgument(boolean expression) {
-        if (!expression) {
-            throw new IllegalArgumentException();
-        }
-    }
-
     /**
      * Ensures that an expression checking an argument is true.
      *
